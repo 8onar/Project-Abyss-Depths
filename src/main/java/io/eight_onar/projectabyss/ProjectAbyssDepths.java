0d1ec3694258b45b41fe1434a75e0ae930970e2b
8onar <lolkek60026@gmail.com>
2026-09-07 00:06:54 +0200

    initial commit

---
+++ src/main/java/io/eight_onar/projectabyss/ProjectAbyssDepths.java
@@ -0,0 +1,54 @@
+package io.eight_onar.projectabyss;
+
+import com.mojang.logging.LogUtils;
+import io.eight_onar.projectabysscontentpack.init.custom.block.PABlocks;
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(ProjectAbyssDepths.MOD_ID)
+public class ProjectAbyssDepths
+{
+    public static final String MOD_ID = "projectabyss_depths";
+
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public ProjectAbyssDepths(FMLJavaModLoadingContext context)
+    {
+        IEventBus modEventBus = context.getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+
+    }
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event)
+    {
+        LOGGER.info("HELLO from server starting");
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+            LOGGER.info("HELLO FROM CLIENT SETUP: " + PABlocks.DRAGON_BLOOD_STAIRS.get().getName());
+            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+        }
+    }
+}
